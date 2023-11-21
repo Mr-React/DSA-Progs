963a560576529f349790541127dc4eed82f50b54
Mr-React <ambureomkar@gmail.com>
2023-11-21 19:35:13 +0530

    Factorial Program

---
+++ programs/Factorial.js
@@ -0,0 +1,13 @@
+/**
+ * Factorial program
+ * Big-O(n)
+ */
+function factorial(n) {
+  let res = 1;
+  for (let i = 2; i <= n; i++) {
+    res = res * i;
+  }
+  return res;
+}
+
+console.log(factorial(4));
