774f2844d332859b2d73b63e78611d7f36a265c6
Mr-React <ambureomkar@gmail.com>
2023-11-21 20:23:23 +0530

    linear search algo

---
+++ Algo/Search/LinearSearch.js
@@ -0,0 +1,16 @@
+/**
+ * Find the given number's index from given array
+ * Big-O(n)
+ */
+
+function LinearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) return i;
+  }
+
+  return -1;
+}
+
+console.log(LinearSearch([-5, 2, 10, 4, 6], 10));
+console.log(LinearSearch([-5, 2, 10, 4, 6], 6));
+console.log(LinearSearch([-5, 2, 10, 4, 6], 20));
