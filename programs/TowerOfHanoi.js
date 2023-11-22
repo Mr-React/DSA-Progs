c5424418938c61f2b76698860046a381111185cf
Mr-React <ambureomkar@gmail.com>
2023-11-22 19:37:08 +0530

    Tower of hanoi program

---
+++ programs/TowerOfHanoi.js
@@ -0,0 +1,16 @@
+/**
+ * Tower of Hanoi Problem
+ */
+
+function TowerOfHanoi(n, fromRod, toRod, usingRod) {
+  if (n === 1) {
+    console.log(`Move Disk 1 from ${fromRod} to ${toRod}`);
+    return;
+  }
+
+  TowerOfHanoi(n - 1, fromRod, usingRod, toRod);
+  console.log(`Move disk ${n} from ${fromRod} to ${toRod}`);
+  TowerOfHanoi(n - 1, usingRod, toRod, fromRod);
+}
+
+TowerOfHanoi(3, "A", "C", "B");
