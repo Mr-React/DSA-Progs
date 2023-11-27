69487ba630d53e95b504ca044fd6af4e32e91d5b
Mr-React <ambureomkar@gmail.com>
2023-11-27 17:09:26 +0530

    Implemented stack using array

---
+++ DS/StackByArray.js
@@ -0,0 +1,43 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  print() {
+    console.log(this.items);
+  }
+}
+
+const stack = new Stack();
+console.log(stack.size());
+console.log(stack.isEmpty());
+stack.push(20);
+stack.push(10);
+stack.push(30);
+console.log(stack.isEmpty());
+console.log(stack.peek());
+stack.print();
+
+console.log(stack.pop());
+console.log(stack.size());
+console.log(stack.peek());
