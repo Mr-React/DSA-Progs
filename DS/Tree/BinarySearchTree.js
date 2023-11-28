f283be3c352077dbce788575b7a2d4aa9ff4f378
Mr-React <ambureomkar@gmail.com>
2023-11-28 17:11:36 +0530

    Implemented BST insert and search methods

---
+++ DS/Tree/BinarySearchTree.js
@@ -0,0 +1,69 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.isEmpty()) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(root, newNode) {
+    if (newNode.value < root.value) {
+      if (root.left === null) {
+        root.left = newNode;
+      } else {
+        this.insertNode(root.left, newNode);
+      }
+    } else {
+      if (root.right === null) {
+        root.right = newNode;
+      } else {
+        this.insertNode(root.right, newNode);
+      }
+    }
+  }
+
+  search(root, value) {
+    if (!root) {
+      return false;
+    } else {
+      if (value === root.value) {
+        return true;
+      } else if (value < root.value) {
+        return this.search(root.left, value);
+      } else {
+        return this.search(root.right, value);
+      }
+    }
+  }
+
+  isEmpty() {
+    return this.root === null;
+  }
+}
+
+const bst = new BinarySearchTree();
+
+console.log("Tree is Empty?: ", bst.isEmpty());
+
+console.log("Insert elements 10,5,15");
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+
+console.log("Search element(5,15,100) in tree exist or not");
+console.log(bst.search(bst.root, 5));
+console.log(bst.search(bst.root, 100));
