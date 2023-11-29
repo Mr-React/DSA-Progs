c9e7a4646adb098ef35a7e9a36be4c138ac3e744
Mr-React <ambureomkar@gmail.com>
2023-11-29 12:25:39 +0530

    Added hasEdge and display method to Graph class

--- DS/Graph/Graph.js
+++ DS/Graph/Graph.js
@@ -21,6 +21,19 @@ class Graph {
     this.adjancencyList[vertex1].add(vertex2);
     this.adjancencyList[vertex2].add(vertex1);
   }
+
+  display() {
+    for (let vertex in this.adjancencyList) {
+      console.log(vertex + " -> " + [...this.adjancencyList[vertex]]);
+    }
+  }
+
+  hasEdge(vertex1, vertex2) {
+    return (
+      this.adjancencyList[vertex1].has(vertex2) &&
+      this.adjancencyList[vertex2].has(vertex1)
+    );
+  }
 }
 
 const graph = new Graph();
@@ -36,3 +49,9 @@ graph.addEdge("A", "B");
 graph.addEdge("B", "C");
 
 console.log(graph.adjancencyList);
+
+console.log("Display all vertices with edges");
+graph.display();
+
+console.log("hasEdge between A & B?: ", graph.hasEdge("A", "B"));
+console.log("hasEdge between A & C?: ", graph.hasEdge("A", "C"));
